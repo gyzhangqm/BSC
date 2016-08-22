016c412564055d9efb57c15a80a7b71b851f5f29
Sanath Keshav <sanathkeshav.mysore@gmail.com>
2016-08-22 21:21:17 +0200

    Added Delaunay Triangulation to plot file visualization

--- hicks_henne/3D/plotfile.m
+++ hicks_henne/3D/plotfile.m
@@ -8,10 +8,27 @@ load updatedl.txt
 Nu = 72;
 Nl = 72;
 panels = 3;
-figure(1);
+
+
+xu = updatedu(:,1);
+xl = updatedl(:,1);
+yu = updatedu(:,2);
+yl = updatedl(:,2);
+zu = updatedu(:,3);
+zl = updatedl(:,3);
+
+% for i = 1:Nu*panels
+%     scatter3(updatedu(i,1), updatedu(i,2), updatedu(i,3),'r.');
+%     scatter3(updatedl(i,1), updatedl(i,2), updatedl(i,3),'r.');
+% end
+% axis equal tight;
+
+U = delaunayTriangulation(xu,yu,zu);
+L = delaunayTriangulation(xl,yl,zl);
+
+faceColor  = [0.6875 0.8750 0.8984];
+figure
 hold on;
-for i = 1:Nu*panels
-    scatter3(updatedu(i,1), updatedu(i,2), updatedu(i,3),'r.');
-    scatter3(updatedl(i,1), updatedl(i,2), updatedl(i,3),'r.');
-end
+tetramesh(L,'FaceColor',faceColor,'FaceAlpha',0.3,'edgecolor','black');
+tetramesh(U,'FaceColor',faceColor,'FaceAlpha',0.3,'edgecolor','black');
 axis equal tight;
